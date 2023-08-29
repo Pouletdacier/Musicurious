78146211e6b1e737d918cf80f0f16895e5464f95
juliette magin <juliette.magin@gmail.com>
2023-08-29 12:27:36 +0200

    basic map, maggl

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @instruments = Instrument.all
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @instruments.geocoded.map do |instrument|
+      {
+        lat: instrument.latitude,
+        lng: instrument.longitude
+      }
+    end
   end
 end
