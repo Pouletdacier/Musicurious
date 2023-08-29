6b7f9296d389c6490e2676932f8e0324a54e2064
Tatiana Parfait <tania.parfait@hotmail.fr>
2023-08-29 12:48:17 +0200

    seed ready

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,3 +1,4 @@
 class Instrument < ApplicationRecord
   has_many :favorites, dependent: :destroy
+  has_many_attached :photos
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,3 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+config.active_storage.service = :cloudinary
