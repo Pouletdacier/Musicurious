a657d83fe1c910ee272d272368efb95b366cbacc
juliette magin <juliette.magin@gmail.com>
2023-08-30 14:51:09 +0200

    basic popups done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
   def home
     @instruments = Instrument.all
-    # The `geocoded` scope filters only flats with coordinates
     @markers = @instruments.geocoded.map do |instrument|
       {
         lat: instrument.latitude,
-        lng: instrument.longitude
+        lng: instrument.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {instrument: instrument})
       }
     end
   end
