c490d62b602c265044c1bfe5d06d75fed718c968
juliette magin <juliette.magin@gmail.com>
2023-09-05 14:54:18 +0200

    quizz basique v2

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -12,7 +12,8 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/satellite-v9"
+      style: "mapbox://styles/mapbox/outdoors-v12"
+      // style: "mapbox://styles/mapbox/satellite-v9"
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
