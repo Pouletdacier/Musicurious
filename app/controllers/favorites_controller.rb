66d007baea4d88ab815454397da49100a3eb1e17
juliette magin <juliette.magin@gmail.com>
2023-09-06 09:28:30 +0200

    les likes marchent pas ouin

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,12 @@
 class FavoritesController < ApplicationController
   def create
+    # @favorites = Favorite.all
     @instrument = Instrument.find(params["instrument_id"])
-    @favorite = Favorite.new
-    @favorite.instrument = @instrument
-    @favorite.user = current_user
-    @favorite.save
-    # gnagnagna if else
+    @favorite = Favorite.new(user: current_user, instrument: @instrument)
+    # @favorite.instrument = @instrument
+    # @favorite.user = current_user
+    if !@favorite.save
+      flash[:notice] = @favorite.errors.full_messages.to_sentence
+    end
   end
 end
